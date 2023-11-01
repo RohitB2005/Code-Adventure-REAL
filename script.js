de3158a554f69a9bd90866f223f5a3fa8560a64b
RohitB2005 <135669356+RohitB2005@users.noreply.github.com>
2023-11-01 15:37:16 +0000

    Changed what happens upon correct and incorrect answers, implemented animations and improved upon detection of correct answers

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const presetAnswers = ["hello world", "hi there", "howdy"]; /* Store multiple correct answers in an array */
+const expectedProperties = ["position: relative;", "top: 63%;", "left: 73%;", "}"]; /* Store each necessary part of the answer in an array */
 
 const submitButton = document.querySelector(".Submit"); /* Setting the submit button to relay to check the code */
+
+const character = document.getElementById("Character");
+
 
 submitButton.addEventListener("click", checkAnswer); /* Runs function when submit button clicked*/
 
 function checkAnswer() {
-  const userAnswer = document.querySelector(".code-input").value.toLowerCase(); /* Convert user's answer to lowercase */
+  const userAnswer = document.querySelector(".code-input").value.toLowerCase(); /* Convert user answer to lower case, avoiding case sensitivity */
 
-  
-  if (presetAnswers.includes(userAnswer)) { /* Check if the users answer matches the preset answer, and what happens when these match */
+  /* Check if all expected properties and values are present in the user's answer */
+  const allPropertiesPresent = expectedProperties.every(property => userAnswer.includes(property));
+
+  /* When a users answer does not incldue all expected properties, display an incorrect message, and if matchng, display correct message */
+  if (allPropertiesPresent) {
     alert("Correct answer!");
+    character.style.top = "63%";
+    character.style.left = "62%";
+
+    character.style.animation = "moveCharacter 2s linear, characterAnimation 0.5s steps(1) 4";
+    character.style.animationFillMode = "forwards";
+
+    /* Hide the sword once the new idle animation begins */
+    setTimeout (() => {
+    const swordItem = document.getElementById("SwordItem");
+    swordItem.style.animation = "hideSword 0s linear";
+    swordItem.style.animationFillMode = "forwards";
+    }, 3000);
+
+    /* Apply the new idle animation after the first animation is finished */
+    setTimeout(() => {
+      character.style.animation = "newIdleAnimation 1.7s infinite steps(5)";
+    }, 3000);
   } else {
-    alert("Incorrect answer. Please try again.");
-  }
-  /* When a users answer does not match the preset, display an incorrect message */
-}
+    submitButton.style.backgroundColor = "red";
+    submitButton.textContent = "Incorrect";
+    /* Revert the button to its original state after 2 seconds */
+        setTimeout(() => {
+          submitButton.style.backgroundColor = "blue";
+          submitButton.textContent = "Submit";
+        }, 2000);
+      }
+    }
 
